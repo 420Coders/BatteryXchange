25a72e77229705061db0f00d5c7bf4f343cbf37b
YoungFrank <fluong95@gmail.com>
2019-02-05 15:00:58 -0500

    just saving all the hash tests

--- models/user.js
+++ models/user.js
@@ -1,9 +1,45 @@
 
 module.exports = function (sequelize, DataTypes) {
+
     const User = sequelize.define("User", {
         email: DataTypes.STRING,
         password: DataTypes.STRING
-    });
+    })
+
+
+
+    //     instanceMethods: {
+    //         comparePassword : function(candidatePassword, cb) {
+    //             bcrypt.compare(candidatePassword, this.getDataValue('password'), function(err, isMatch) {
+    //                 if(err) return cb(err);
+    //                 cb(null, isMatch);
+    //             });
+    //         },
+    //         setToken: function(){
+
+
+    //         },
+    //         getFullname: function() {
+    //             return [this.firstname, this.lastname].join(' ');
+    //         }
+    //     }
+    // }
+
+
+
+    // User.pre("create", async function(next) {
+    //     try {
+    //         //generate a salt
+    //         const salt = await bcrypt.genSalt(10);
+    //         const passwordHash = await bcrypt.hash(this.password, salt);
+
+    //         console.log("salt: " + salt);
+    //         console.log("Normal Password: " + this.password);
+    //         console.log("Hashed Password: " + passwordHash);
+    //     } catch(error) {
+    //         next(error);
+    //     }
+    // });
 
     return User;
 }
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,17 +1,44 @@
 const db = require("../models");
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 // const passport = require("../utils/passport");
+var bcrypt = require('bcryptjs');
 
 module.exports = function (app) {
+
+    //creating a new user
     app.post("/api/newUser", (req, res) => {
+
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync(req.body.password, salt);
+
         db.User.create({
             email: req.body.email,
-            password: req.body.password
+            password: hash
         })
-        .then(function (dbUser) {
-            res.json(dbUser)
-        });
+            .then(function (dbUser) {
+                res.json(dbUser)
+            });
     });
 
-    
+
+    //logging in
+    app.post("/api/login", (req, res) => {
+        db.User.findOne({"username" : req.body.username}, function(err, User) {
+            const response = {
+                status : 401,
+                message : "failed"
+            }
+            if(err) {
+                response.status = 401;
+                response.message = "failed";
+            }
+            if(bcrypt.compareSync(req.body.password, User.password)) {
+                response.status = 201;
+                response.message = "success";
+            } else {
+                response.status = 401;
+                response.message = "failed";
+            }
+        })
+    })
 }
