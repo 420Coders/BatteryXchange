898ed14cebf590170d05b31826c2eb5353c934f7
YoungFrank <fluong95@gmail.com>
2019-02-07 17:39:50 -0500

    added client side serverjs

--- client/src/pages/ThankYouPage.js
+++ client/src/pages/ThankYouPage.js
@@ -1,7 +1,21 @@
 import React, {Component} from 'react';
 
-
+import "./ThanksYouPage.css";
 
 class ThankYouPage extends Component {
-    
+
+
+    render() {
+
+        return (
+            <div>
+                <h1>Thank You For Using Our Product!</h1>
+                <br></br>
+                <br></br>
+                <h3>We would love to hear your feedback on the survey below!</h3>
+            </div>
+        )
+    }
 }
+
+export default ThankYouPage;
