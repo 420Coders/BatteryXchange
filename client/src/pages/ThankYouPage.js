110f8872fd4dd5bead24fafca8193d3a55dec914
vaphi <vphi@uncc.edu>
2019-02-08 20:21:32 -0500

    testing 1cent

--- client/src/pages/ThankYouPage.js
+++ client/src/pages/ThankYouPage.js
@@ -17,19 +17,19 @@ class ThankYouPage extends Component {
                 <p>BATTERYXCHANGE</p>
             </div>
             <div class="alert alert" role="alert">
-                <hr/>   
-                <h1 class="alert-heading">Thank You For Using Our Product!</h1>
                 <hr/>
-                <h4 class="alert-heading">We would love to hear your feedback on the survey below!</h4>
-                 <br/>
+                <hr/>
+                <p class="alert-heading">Thank you for your payment. Your transaction has been completed, and a receipt for your purchase has been emailed to you. Log into your PayPal account to view transaction details.</p>
+                <hr/>   
+                <hr/>
+                <h5 class="alert-heading">We would love to hear your feedback on the survey below!</h5>
                  <button type="button" class="btn btn-success"> 
                     <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSeuVCbNoJwanHHKaJC-TN7YzR4s905ZLovhQjgGmJBY3EvNlQ/viewform?usp=sf_link">Survey!</a>
                 </button>
-                <hr/>
             </div>
             <div class="alert alert-danger" role="alert">
                  <hr/>   
-                 <h4 class="alert-heading">After 30 minutes are up, please return the battery to it's original location</h4>
+                 <h5 class="alert-heading">After 30 minutes are up, please return the battery to it's original location</h5>
                 <hr/>
             </div>   
             </div>                     
